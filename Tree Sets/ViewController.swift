24d97f1034f18955f8382565329919a2b15a216b
Cristianno Vieira <cmartins@ic.unicamp.br>
2016-12-28 20:09:05 -0200

    Initial Commit

---
+++ Tree Sets/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tree Sets
+//
+//  Created by Cristianno Vieira on 28/12/16.
+//  Copyright © 2016 Cristianno Vieira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
